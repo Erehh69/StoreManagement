837ef17aecce36e24be004ff5666e6f13b3c9ab0
Erehh69 <92562056+Erehh69@users.noreply.github.com>
2024-11-24 21:29:27 +0800

    full ver
    full

--- StoreManagement/Data/StoreContext.cs
+++ StoreManagement/Data/StoreContext.cs
@@ -26,7 +26,9 @@ namespace StoreManagementApp.Data
                 .HasOne(t => t.Product) // Each transaction is associated with a product
                 .WithMany(p => p.Transactions) // Each product can have many transactions
                 .HasForeignKey(t => t.ProductId) // Foreign key is ProductId in Transaction
+                .OnDelete(DeleteBehavior.Cascade)
                 .IsRequired(false); // Make this relationship optional (no need for a transaction on product creation)
+
 
             // Mark Report as a keyless entity (Report doesn't have a primary key)
             modelBuilder.Entity<Report>().HasNoKey();
---
+++ StoreManagement/Migrations/20241119073050_ConfigureCascadeDelete.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Products_ProductId",
+                table: "Transactions");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Products_ProductId",
+                table: "Transactions",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Products_ProductId",
+                table: "Transactions");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Products_ProductId",
+                table: "Transactions",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
